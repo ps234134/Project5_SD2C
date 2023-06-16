65de77c8258c0ed7d42774cff9c23464d385c0d2
ps227374 <ps227374@summacollege.nl>
2023-06-16 18:27:29 +0200

    usertable seeder made

---
+++ Project5_SD2C/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Faker\Factory as Faker;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $faker = Faker::create();
+
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        // Generate random users with the same encrypted password
+        for ($i = 1; $i <= 10; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'password' => Hash::make('password'),
+            ]);
+        }
+    }
+}
